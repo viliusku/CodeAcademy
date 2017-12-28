c05d14f55b454bc38faa0b48e100bbf31fbda304
viliusku <viliusku@gmail.com>
2017-12-28 19:09:11 +0200

    vk

--- kontroliniai/10.php
+++ kontroliniai/10.php
@@ -3,27 +3,27 @@
 Sukurkite PHP skriptą, kuriame aprašykime klasę numberList, kurioje būtų viena savybė ‐ $numbers, kuri bus masyvas, taip pat būtų metodas add($number), kuris prideda naują skaičių į masyvą sąvybę $numbers. Taip pat sukurkite metodą average(), kuris grąžintų masyve sąvybėje esančių elementų matematinį vidurkį
 */
 
-class numberList {
-    public $numbers;
-    function add($number){
-        if (!isset($this->numbers)) $this->numbers = [];
-        $this->numbers[] = $number;
+class numberList { // klases aprašas
+    public $numbers; // savybė, kurioje bus talpinamas skaičių masyvas
+    function add($number){ // funkcija įvesti skaičių ir patalpinti į masyvą (savybę)
+        if (!isset($this->numbers)) $this->numbers = []; // jeigu savybė dar nėra naudota, tai įrašyti į ją tuščia paprastą masyvą
+        $this->numbers[] = $number; // patalpinti į savybę (masyvą) skaičių
     }
-    function average(){
-        $sum = 0;
-        for ($i=0; $i<count($this->numbers); $i++){
-            $sum += $this->numbers[$i];
+    function average(){ // funkcija skaičių vidurkio skaičiavimui
+        $sum = 0; // prieš skaičių sumos skaičiavime nustatome sumą = 0
+        for ($i=0; $i<count($this->numbers); $i++){ // imame po skaičius iš savybės (masyvo) po vieną
+            $sum += $this->numbers[$i]; // sumuojame eilinį skaičių iš masyvo prie sumos
         }
-        return $sum / count($this->numbers);
+        return $sum / count($this->numbers); // padaliniame sumą iš skaičių masyve skaičiaus ir gražiname kaip funkcijos rezultatą
     }
 }
 
-$objektas = new numberList();
+$objektas = new numberList(); // sukuriame klasės objekto egzempliorių
 
-$objektas->add(1);
-$objektas->add(13);
-$objektas->add(1103);
-$objektas->add(133);
-$objektas->add(555);
+$objektas->add(1); // įdedame skaičių
+$objektas->add(13); // įdedame skaičių
+$objektas->add(1103); // įdedame skaičių
+$objektas->add(133); // įdedame skaičių
+$objektas->add(555); // įdedame skaičių
 
-echo $objektas->average();
+echo $objektas->average(); // skaičiuojame vidurkį ir parodome
