dc4f1e3b53f98dd6bedf853eb03ac6d38b3890b3
viliusku <viliusku@gmail.com>
2018-01-01 21:54:03 +0200

    vk

---
+++ kontroliniai/10.4.php
@@ -0,0 +1,4 @@
+<?php
+/*
+Sukurkite PHP skriptą, kuriame aprašykime klasę textList, kurioje būtų viena savybė ‐ $texts, kuri bus masyvas, taip pat būtų metodas add($text), kuris prideda naują tekstą į masyvą sąvybę $texts. Taip pat sukurkite metodą symbols(), kuris grąžintų masyve sąvybėje esančių elementų simbolių skaičių
+*/
