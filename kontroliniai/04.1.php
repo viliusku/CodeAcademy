27c18204d892ab92200ae10d136ddf0d88dc15a6
viliusku <viliusku@gmail.com>
2018-01-02 19:30:11 +0200

    vk

--- kontroliniai/04.1.php
+++ kontroliniai/04.1.php
@@ -2,3 +2,10 @@
 /*
 Sukurkite PHP skriptą, kuriame būtų aprašyti trys kintamieji ir ekrane būtų išvestas tų kintamųjų kvadratų suma.
 */
+
+$a = 1;
+$b = 2;
+$c = 3;
+$d = 4;
+
+echo $a**2 + $b**2 + $c**2 + $d**2;
