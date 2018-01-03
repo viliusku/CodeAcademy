2bb303b64270c3c0980662250ace05e99d329103
viliusku <viliusku@gmail.com>
2018-01-03 11:16:52 +0200

    vk

---
+++ kontroliniai/06.4.php
@@ -0,0 +1,4 @@
+<?php
+/*
+Sukurkite PHP skriptą, kuriame būtų aprašyta funkcija, kuriai padavus du parametrus ‐ skaicius, funkcija turi sudeti skaicius, pakelti kvadratu ir padalinti is 3. Rezultata grazinti. Iskviesti funkcija perduodant du parametrus (skaicius) ir parodyti rezultata.
+*/
---
+++ kontroliniai/07.4.php
@@ -0,0 +1,4 @@
+<?php
+/*
+Sukurkite PHP skriptą, kuriame būtų aprašytas tekstas sudarytas iš žodžių. Suskaidykite tekstą į žodžius ir sukelkite į masyvą. Panaudodami foreach ciklą ‐ pakeiskite visus masyvo elementus ‐ paversdami tekstą didžiosiomis raidemis, gale teksto prijunkite simbolį '*'. Sujunkite visus masyvo elementus į vieną teksto eilutę sujungdami juos horizontaliais '-' brūšniais.
+*/
