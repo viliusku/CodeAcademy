b194c6210295f9ec116f4aa24a5fb01d80d2ce86
viliusku <viliusku@gmail.com>
2018-01-05 15:37:08 +0200

    vk

--- kontroliniai/10.8.php
+++ kontroliniai/10.8.php
@@ -1,4 +1,38 @@
 <?php
 /*
-Sukurkite PHP skriptą, kuriame aprašykime klasę numberList, kurioje būtų viena savybė ‐ $numbers, kuri bus masyvas, taip pat būtų metodas add($number), kuris prideda naują skaičių į masyvą sąvybę $numbers. Sukurkite metodą delMin(), kuris pašalina mažiausią masyve sąvybėje esantį skaičių. sukurkite funkciją getInfo(), kuris išveda skaičius esančius savybėje.
+Sukurkite PHP skriptą, kuriame aprašykime klasę numberList, kurioje būtų viena savybė ‐ $numbers, kuri bus masyvas, taip pat būtų metodas add($number), kuris prideda naują skaičių į masyvą sąvybę $numbers. Sukurkite metodą delMin(), kuris pašalina mažiausią masyve sąvybėje esantį skaičių. Sukurkite funkciją getInfo(), kuris išveda skaičius esančius savybėje.
 */
+
+class numberList {
+    public $numbers = [];
+    function add($number){
+        $this->numbers[] = $number;
+    }
+    function delMin(){
+        $min = $this->numbers[0];
+        $min_i = 0;
+        for ($i=1; $i<count($this->numbers); $i++){
+            if ($this->numbers[$i] < $min){
+                $min = $this->numbers[$i];
+                $min_i = $i;
+            }
+        }
+        unset($this->numbers[$min_i]);
+    }
+    function getInfo(){
+        foreach ($this->numbers as $number){
+            echo $number . ' ';
+        }
+    }
+}
+
+$a = new numberList();
+$a->add(1);
+$a->add(10);
+$a->add(-1);
+$a->add(15);
+$a->add(3);
+$a->getInfo();
+echo '<br>';
+$a->delMin();
+$a->getInfo();
