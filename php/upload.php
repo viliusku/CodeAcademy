274e54260725321bebd6125b274f2b53e2bfa398
viliusku <viliusku@gmail.com>
2018-01-08 09:06:50 +0200

    vk

---
+++ php/upload.php
@@ -0,0 +1,36 @@
+<?php
+
+var_dump($_FILES);
+$target_dir = $_SERVER['DOCUMENT_ROOT'] . '/uploads';
+$target_file = $target_dir . '/' . basename($_FILES["fileToUpload"]["name"]);
+$uploadOk = 0;
+$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+// Check if image file is a actual image or fake image
+if(isset($_POST["submit"])) {
+    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+    var_dump($check);
+    if($check !== false) {
+        echo "File is an image - " . $check["mime"] . ".<br>";
+        $uploadOk = 1;
+    } else {
+        echo "File is not an image.<br>";
+    }
+}
+
+// Allow certain file formats
+if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+    && $imageFileType != "gif" ) {
+    echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.<br>";
+    $uploadOk = 0;
+}
+// Check if $uploadOk is set to 0 by an error
+if ($uploadOk == 0) {
+    echo "Sorry, your file was not uploaded.<br>";
+// if everything is ok, try to upload file
+} else {
+    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+        echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.<br>";
+    } else {
+        echo "Sorry, there was an error uploading your file.<br>";
+    }
+}
