605eaf202b899a3bb065c7668f5127ea4ca73891
viliusku <viliusku@gmail.com>
2018-01-08 11:30:52 +0200

    vk

--- kontroliniai/darbas/index.php
+++ kontroliniai/darbas/index.php
@@ -1,22 +1,20 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
+<html lang="lt">
+<head   >
     <meta charset="UTF-8">
     <title>Title</title>
     <!-- Latest compiled and minified CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css">
-
     <!-- jQuery library -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-
     <!-- Popper JS -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.6/umd/popper.min.js"></script>
-
     <!-- Latest compiled JavaScript -->
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js"></script>
-    <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" type="text/css">
 
-    <link rel="stylesheet" href="auto.css" type="text/css">
+    <link rel="stylesheet" href="/kontroliniai/darbas/auto.css" type="text/css">
 
 </head>
 <body>
@@ -25,9 +23,16 @@
     <div class="site-header">
         <table class="mx-auto w-100">
             <tr>
-                <td>aaa</td>
-                <td>bbb</td>
-                <td>ccc</td>
+                <td><img class="site-logo" src="http://test.lt/kontroliniai/darbas/images/auto-nuoma.png" alt="Automobilių nuomos internetinė informacinė sistema | UAB Auto 7/24"></td>
+                <td>
+                    <h1 class="site-title">Automobilių nuoma 7/24</h1>
+                    <div class="row site-header-top-grid">
+                        <div class="col p-0"><button class="btn m-0 p-0 pb-1 pl-2 pr-2"><i class="fa fa-phone pr-1" aria-hidden="true"></i>+370 687 44987</button></div>
+                        <div class="col p-0"></div>
+                        <div class="col p-0"></div>
+                        <div class="col p-0"></div>
+                    </div>
+                </td>
             </tr>
         </table>
     </div>
