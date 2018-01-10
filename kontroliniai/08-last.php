8f66d7a20eb3408b71bae5871f882d369928d2eb
viliusku <viliusku@gmail.com>
2018-01-10 22:59:22 +0200

    vk

---
+++ kontroliniai/08-last.php
@@ -0,0 +1,4 @@
+<?php
+/*
+Sukurkite PHP skriptą, kuriame būtų aprašyta klasė “salis”, kuri turi savybes ‐ pavadinimas, sostine, gyventojai. Sukurkite standartinį klasės __construct metodą, kuriam perdavus tris parametrus ‐ pavadinimas, sostine, gyventojai ‐ perduotus parametrus padėtų į savo savybes.
+*/
