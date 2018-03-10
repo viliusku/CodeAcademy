77abba71087641c02c43771b1927ba1cadebcada
Kernius <kernius.matulis@gmail.com>
2018-03-10 13:04:59 +0200

    Commit

---
+++ php-002/php/savarankiskas-funkcija-masyvas-4.php
@@ -0,0 +1,6 @@
+<?php
+/**
+Yra masyvas su tekstais. Padaryti funkciją, kuri pakeistu kiekvieno masyvo elemento tekstą į teksto ilgį (skaičių) . Funkcija turi gražinti pakeistą masyvą. Pademonstruoti veikimą.
+
+ */
+
---
+++ php-002/php/savarankiskas-funkcija-masyvas-5.php
@@ -0,0 +1,7 @@
+<?php
+/**
+Yra masyvas su tekstais. Padaryti funkciją, kuri pakeistu kiekvieno masyvo elemento tekstą į asociatyviną masyvą, kurio duomenys turėtų būti sekantys: 1)raktas 'tekstas', reikšmė originalus tekstas, 2) raktas 'ilgis', reikšme teksto ilgis. Funkcija turi gražinti pakeistą masyvą. Pademonstruoti veikimą.
+
+ */
+
+
