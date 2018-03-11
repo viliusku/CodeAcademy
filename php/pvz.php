6c24221cc3183d1c477531f0d9454aecf1a58ee7
viliusku <viliusku@gmail.com>
2018-03-11 21:21:40 +0200

    vk

---
+++ php/pvz.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+    <title>test</title>
+</head>
+<body>
+
+Cia prasideda PHP:<br>
+<?php
+echo "My first PHP script!";
+$abc = 3;
+$abc += 5;
+echo '<br>' . $abc;
+?>
+
+</body>
+</html>
