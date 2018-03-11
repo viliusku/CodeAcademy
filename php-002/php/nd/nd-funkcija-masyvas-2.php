f4247e82e9fb92216b26db0f8bace31ac957ee69
viliusku <viliusku@gmail.com>
2018-03-11 11:57:55 +0200

    vk

---
+++ php-002/php/nd/nd-funkcija-masyvas-2.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ *
+ *
+Yra masyvas su tekstais. Padaryti funkciją, kuri masyvo tekstuose pakeistų nurodytą raidę į tarpą. Funkcijos parametrai: masyvas ir nurodyta raidė. Funkcija turi gražinti pakeistą masyvą. Pademonstruoti veikimą.
+ *
+ */
+
+
