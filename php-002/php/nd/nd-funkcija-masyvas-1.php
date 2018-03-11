f20f28d6db8295e383c82bdec450b9a1851ff09c
viliusku <viliusku@gmail.com>
2018-03-11 11:53:50 +0200

    vk

---
+++ php-002/php/nd/nd-funkcija-masyvas-1.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ *
+ *
+Yra masyvas su tekstais. Padaryti funkciją, kuri vietoje tekstų į masyvą įrašytų tekstų pirmąsias raides. Funkcijos parametras - masyvas. Funkcija turi gražinti pakeistą masyvą. Pademonstruoti veikimą.
+ *
+ */
+
+
