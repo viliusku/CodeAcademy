bfc62ed712b1967173e6128c1d0a9bfad60d67b5
viliusku <viliusku@gmail.com>
2018-03-11 12:04:19 +0200

    vk

---
+++ php-002/php/nd/nd-funkcija-masyvas-3.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ *
+ *
+Yra du masyvai su skaičiais. Padaryti funkciją, kuri pakeistų nuliais visus pirmo masyvo elementus kurie egzistuoja ir antrame masyve (sutampančius). Funkcijos parametrai: du masyvai. Funkcija turi gražinti pakeistą pirmą masyvą. Pademonstruoti veikimą.
+ *
+ */
+
+
