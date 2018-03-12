34840dd3071cd42e72e2fd1e93b3c4ed2fd52d4f
Kernius <kernius.matulis@gmail.com>
2018-03-12 15:58:37 +0200

    Commit

---
+++ php-002/php/kaikas.php
@@ -0,0 +1,14 @@
+<?php
+
+//var_dump($_SERVER);
+//phpinfo();
+//$s = "echo 'labas';";
+//eval($s);
+
+//$s = "<div>aaaa</div>";
+// $lt;
+//echo htmlspecialchars($s);
+
+//echo uniqid();
+echo md5('labas vakaras') . '<br>';
+echo md5('labas vakaras aaa');
