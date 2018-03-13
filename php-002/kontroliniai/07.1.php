c9e66a986ec5f8da817debbdf64bfa3583bba095
viliusku <viliusku@gmail.com>
2018-03-13 23:35:49 +0200

    vk

---
+++ php-002/kontroliniai/07.1.php
@@ -0,0 +1,4 @@
+<?php
+/**
+Sukurkite PHP skriptą, kuriame būtų aprašytas asociatyvinis masyvas iš 5 elementų ‐ teksto eilučių. Panaudodami foreach ciklą pakeiskite visus masyvo elementus ‐ paversdami tekstą didžiosiomis raidėmis, o paskutinę raidę - pašalinkite.
+*/
---
+++ php-002/kontroliniai/10.1.php
@@ -0,0 +1,4 @@
+<?php
+/*
+Sukurti klasę turgus. Sukurti funkciją atomobilis, su kuria būtų galima pateikti automobilį į turgų. Funkcijos parametrai: gamintojas, modelis, metai, kaina. Automobilių informacijų talpinti į klasės savybę automobiliai (masyvas). Sukurti funkciją minKaina, kuri surastų automobilį su mažiausia kaina.
+*/
