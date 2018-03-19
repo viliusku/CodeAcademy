787dfd25a30135da664459594fa251b0c28d3ca6
Kernius <kernius.matulis@gmail.com>
2018-03-19 15:56:07 +0200

    Commit

---
+++ php-002/jquery/next-ajax.php
@@ -0,0 +1,9 @@
+<?php
+
+$m = [
+    [1, 'Jonas', 1000],
+    [2, 'Petras', 2000],
+    [2, 'Antanas', 3000]
+];
+
+echo json_encode($m);
--- php-002/jquery/simple.js
+++ php-002/jquery/simple.js
@@ -13,4 +13,13 @@ $(document).ready(function(){
     $(".spn").parent().css('color', 'blue');
     $(".spn").parent().css('color', 'blue');
     $(".spn").closest('div').find('p').eq(1).css('color', 'red');
+    var a = $(".spn").closest('div').find('p').eq(2);
+    $(a).text('tekstas');
+    var $b = $("#pvz").closest('div').find('p').eq(2);
+    $('#aaa').text($b.text()).css('color', 'red');
+    $('#div').html('<p style="color:blue">paragrafas div</p>').css('border', '1px solid blue');
+    // savarankisko 1 sprendimas
+    $('ul>li').eq(0).css('color', 'red');
+    $('ul>li').eq(1).text('antras elements');
+    $('ul>li').eq(2).html($('ul>li').eq(2).text() + '<span style="font-weight:bold"> antras elementas</span>');
 });
