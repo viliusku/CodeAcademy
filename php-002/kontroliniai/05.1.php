e0bfc22dc61de06e770dc98732a79663deab217a
Kernius <kernius.matulis@gmail.com>
2018-03-20 10:50:09 +0200

    Commit

--- php-002/kontroliniai/05.1.php
+++ php-002/kontroliniai/05.1.php
@@ -2,3 +2,12 @@
 /*
 Sukurkite PHP skriptą, kuriame būtų aprašytas vienmatis masyvas, aprašantis vieną asmenį naudojant raktus, pavyzdžiui “lytis” => “vyras”. Masyve turi būti laukeliai ‐ vardas, pavarde, gimimo metai, lytis.
 */
+
+$m = [
+    'vardas' => 'Jonas',
+    'pavarde' => 'Jonaitis',
+    'gimimo metai' => 1990,
+    'lytis' => 'vyras'
+];
+
+var_dump($m);
--- php-002/kontroliniai/08.2.php
+++ php-002/kontroliniai/08.2.php
@@ -2,3 +2,17 @@
 /*
 Sukurkite PHP skriptą, kuriame būtų aprašyta klasė “codeAcademy”, kuri turi savybes ‐ data, skaicius, auditorija. Sukurkite standartinį klasės __construct metodą, kuriam perdavus tris parametrus ‐ data, skaicius, auditorija ‐ perduotus parametrus padėtų į savo savybes.
 */
+
+class codeAcademy {
+    public $data;
+    public $skaicius;
+    public $auditorija;
+    function __construct($d, $s, $a){
+        $this->data = $d;
+        $this->skaicius = $s;
+        $this->auditorija = $a;
+    }
+}
+
+$a = new codeAcademy('2017-12-10', 11, '3sale');
+var_dump($a);
