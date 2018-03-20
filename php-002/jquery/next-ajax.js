55040f9f240d07c375c842db0323fdbc6dcd9acf
Kernius <kernius.matulis@gmail.com>
2018-03-20 09:08:37 +0200

    Commit

--- php-002/jquery/next-ajax.js
+++ php-002/jquery/next-ajax.js
@@ -1,13 +1,15 @@
 $(document).ready(function(){
-    $.getJSON('next-ajax.php', function(data){
-       var tb = $('#lentele tbody');
-       for (var i=0; i<data.length; i++){
-           var s = '<tr>';
-           s += '<td>' + data[i][0] + '</td>';
-           s += '<td>' + data[i][1] + '</td>';
-           s += '<td>' + data[i][2] + '</td>';
-           s += '</tr>';
-           $(tb).append(s);
-       }
+    $('#bt').click(function(){
+        $.getJSON('next-ajax.php?aaa=5', function(data){
+           var tb = $('#lentele tbody');
+           for (var i=0; i<data.length; i++){
+               var s = '<tr>';
+               s += '<td>' + data[i][0] + '</td>';
+               s += '<td>' + data[i][1] + '</td>';
+               s += '<td>' + data[i][2] + '</td>';
+               s += '</tr>';
+               $(tb).append(s);
+           }
+        });
     });
 });
