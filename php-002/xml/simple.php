f2eae0e43b25e4f84079bfda45cecce3a88dc525
Kernius <kernius.matulis@gmail.com>
2018-03-22 15:43:46 +0200

    Commit

--- php-002/xml/simple.php
+++ php-002/xml/simple.php
@@ -9,15 +9,28 @@
 $s = <<<EOT
 <?xml version="1.0" encoding="UTF-8"?>
 <data>
-    <lubos>Aukstos</lubos>
-    <lubos>Zemos</lubos>
-    <lubos>Vidutines</lubos>
+    <lubos hight="3">Aukstos</lubos>
+    <lubos hight="2">Zemos</lubos>
+    <lubos hight="4">
+        Vidutines
+         <lubos hight="5">
+            xxx
+        </lubos>
+    </lubos>
 </data>
 EOT;
 
 //echo htmlspecialchars($s);
 $xml = simplexml_load_string($s);
-$x = $xml->xpath("lubos");
+$x = $xml->xpath("//lubos");
 foreach($x as $item){
-    echo $item . "<br>";
+    echo $item . ' ' . $item['hight'] . "<br>";
 }
+echo '<hr>';
+$x = $xml->xpath("lubos[@hight>2]");
+foreach($x as $item){
+    echo $item . ' ' . $item['hight'] . "<br>";
+}
+
+$ss = $xml->asXML();
+echo htmlspecialchars($ss);
