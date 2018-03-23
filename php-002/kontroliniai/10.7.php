f50f7b3c8fdec8266c2109cf08d3aeec8fcd05aa
Kernius <kernius.matulis@gmail.com>
2018-03-23 11:02:59 +0200

    Commit

--- php-002/kontroliniai/10.7.php
+++ php-002/kontroliniai/10.7.php
@@ -1,4 +1,5 @@
 <?php
 /*
 Sukurkite PHP skriptą, kuriame aprašykite klasę lentele, kurioje būtų viena savybė ‐ $table, kuri bus dvimatis masyvas, taip pat būtų metodas add, kurio parametras yra 3 skaičių masyvas. Patalpinti gautą masyvą į savybės %table masyvą. Sukurkite metodą get(),kuris paskaičiuoja kiekvienoje lentelės eilutėje esančių elementų vidurkį ir jų patalpina į eilutės pabaigą. Išvesti savybės $table masyvą HTML lentele. Pademonstruokite veikimą.
+
 */
