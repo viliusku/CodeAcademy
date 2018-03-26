da05cda74e566167e8c9f8d2c0ce1e33630905c4
Kernius <kernius.matulis@gmail.com>
2018-03-26 13:51:56 +0300

    Commit

---
+++ php-002/auto/header.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Auto</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <script defer src="https://use.fontawesome.com/releases/v5.0.8/js/all.js" integrity="sha384-SlE991lGASHoBfWbelyBPLsUlwY1GwNDJo3jSJO04KZ33K2bwfV9YBauFfnzvynJ" crossorigin="anonymous"></script>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+
+    <!-- jQuery library -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+    <!-- Popper JS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+
+    <!-- Latest compiled JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="auto.css">
+</head>
+<body>
+<div class="container">
+    <div class="header">
+        <div class="row header-top">
+            <div class="col header-top-fb"><i class="fab fa-facebook-square"></i></div>
+            <div class="col header-top-phone"><i class="fas fa-phone"></i> +370 687 44987</div>
+        </div>
+        <div class="header-menu mx-auto">
+            <nav class="navbar navbar-expand-sm bg-dark navbar-dark rounded">
+                <!-- Brand -->
+                <a class="navbar-brand" href="#"><img class="rounded" src="logo.jpg"></a>
+                <!-- Links -->
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Link 1</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Link 2</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Link 3</a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+    <div class="body">
