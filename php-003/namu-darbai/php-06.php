6cda10ea6be917ac9d2ff100d6724abb885a0e6e
viliusku <viliusku@gmail.com>
2018-04-15 00:23:26 +0300

    vk

---
+++ php-003/namu-darbai/php-06.php
@@ -0,0 +1,4 @@
+<?php
+/**
+Sukurkite tekstų masyvą. Panaudodami for ciklą suraskite žodžius kuriuose yra simbolis "a" ir sudėkite juos į kitą (naują) masyvą.  Pademonstruokite rezultatą.
+ */
---
+++ php-003/namu-darbai/php-07.php
@@ -0,0 +1,4 @@
+<?php
+/**
+Panaudodami while ciklą sukurkite atsitikinių skaičių (intervale nuo 1 iki 100) masyvą. Panaudodami foreach ciklą suraskite mažiausią nelyginį skaičių masyve.  Pademonstruokite rezultatą.
+ */
