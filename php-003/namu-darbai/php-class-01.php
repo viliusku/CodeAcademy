bde2c9457458725aeafb390a3c8ec2ea09ea7617
viliusku <viliusku@gmail.com>
2018-04-19 14:45:46 +0300

    vk

---
+++ php-003/namu-darbai/php-class-01.php
@@ -0,0 +1,4 @@
+<?php
+/*
+Sukurkite PHP skriptą, kuriame būtų aprašyta klasė “automobilis”, kuri turi savybes ‐ gamintojas, modelis, metai. Sukurkite standartinį klasės __construct metodą, kuriam perdavus tris parametrus ‐ gamintojas, modelis, metai ‐ perduotus parametrus padėtų į savo savybes
+*/
