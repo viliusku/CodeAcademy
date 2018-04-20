1b6cb0d9ea7d57182d807169da4a5c7e4f1ffbae
Kernius <kernius.matulis@gmail.com>
2018-04-20 19:46:59 +0300

    Commit

---
+++ php-003/kontroliniai/php-02.php
@@ -0,0 +1,4 @@
+<?php
+/**
+Sukurti klasę svečiai. Klaseję turi būti savybė - svečių sąrašas. Sukurti klsės metodą, su kuriuo būtų galima papildyti svečių sąrašą. Informacija apie svečią: vardas, lytis, patiekalas, amzius. Sukurti klasės metodą svečių sąrašui suformuoti.
+ */
