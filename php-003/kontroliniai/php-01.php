696c5b5e15c7d75b77450075e1d8c7405bda85ca
Kernius <kernius.matulis@gmail.com>
2018-04-20 18:48:25 +0300

    Commit

---
+++ php-003/kontroliniai/php-01.php
@@ -0,0 +1,3 @@
+<?php
+/**
+Sukurti CodeAcademy klasę, kuriose yra savybė - kursų masyvas. Sukurti funkciją, kuri papildytu tą masyvą asociatyviniu kurso masyvu, kurio raktai: data, kiekis, pavadinimas. Sukurti funkciją, kuri išvestų informacija apie kursus lentele. */
