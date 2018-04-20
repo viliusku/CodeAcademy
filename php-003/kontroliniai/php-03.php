be562e8aca7a81d3ce277996cebf9547b11e47d5
Kernius <kernius.matulis@gmail.com>
2018-04-20 21:08:39 +0300

    Commit

--- php-003/kontroliniai/php-03.php
+++ php-003/kontroliniai/php-03.php
@@ -1,4 +1,4 @@
 <?php
 /**
-Sukurti klasę, kuri turi savybę - skaičių sąrašas. Sukurti metodą, kuris sugeneruotų atsitiktinį skaičių intervale nuo 1 iki 100 ir patalpintų į sąrašą, o po to surūšiuotų sąrašą. Sukurti metodą, kuris gražiną sugeneruotų skaičių aritmetitinį vidurkį. Sukurti metodą, kuris išveda masyvo elementus tekstu, kur masyvo elementai atskirti kableliais. Pademonstruoti veikimą.
+Sukurti klasę, kuri turi savybę - skaičių sąrašas. Sukurti metodą, kuris sugeneruotų atsitiktinį skaičių intervale nuo 1 iki 100 ir patalpintų į sąrašą, o po to surūšiuotų sąrašą. Sukurti metodą, kuris gražiną sugeneruotų skaičių aritmetinį vidurkį. Sukurti metodą, kuris išveda masyvo elementus tekstu, kur masyvo elementai atskirti kableliais. Pademonstruoti veikimą.
  */
