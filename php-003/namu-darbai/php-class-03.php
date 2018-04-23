461db851ddb06eee1d6291997f2e8aa36689fb5f
Kernius <kernius.matulis@gmail.com>
2018-04-23 19:20:03 +0300

    Commit

--- php-003/namu-darbai/php-class-03.php
+++ php-003/namu-darbai/php-class-03.php
@@ -2,3 +2,28 @@
 /**
 Sukurti klasę darbuotojai, kurioje yra savybė skirta darbuotojų sąrašui saugoti. Sukurti metodą, kurio pagalba galima patalpinti darbuotojo duomenis į sąrašą: asmens kodas, pavarde ir vardas, pareigos, atlyginimas. Sukurti metodą, kurio pagalba galima būtų pašalinti darbuotojo duomenis iš sąrašo pagal asmens kodą. Pademonstruoti veikimą.
  */
+
+class darbuotojai {
+    public $sar = [];
+    function add($ak, $pv, $va, $pa, $at){
+        $this->sar[$ak] = [
+            'ak' => $ak,
+            'pv' => $pv,
+            'va' => $va,
+            'pa' => $pa,
+            'at' => $at
+        ];
+    }
+    function del($ak){
+        unset($this->sar[$ak]);
+    }
+}
+
+$o = new darbuotojai();
+$o->add('1234567890', 'Jonaitis', 'Jonas', 'darbininkas', 500);
+$o->add('1234567891', 'Petraitis', 'Petras', 'Direktorius', 2000);
+$o->add('1234567892', 'Antanaitis', 'Antanas', 'Vadybininkas', 1000);
+
+var_dump($o);
+$o->del('1234567891');
+var_dump($o);
--- php-003/namu-darbai/php-class-04.php
+++ php-003/namu-darbai/php-class-04.php
@@ -2,3 +2,41 @@
 /**
 Sukurti klasę, kurioje yra įmonės akcininkų sąrašas. Sukurti metodą, kurio pagalba galima į sąrašą įvesti įmonės akcininkų duomenis: vardas, pavardė, akcijų skaičius. Akcininkų sąrašą surūšiuoti pagal turimų akcijų skaičių mažėjančia tvarka. Sukurti metodą, kuris išvestu akcininikų sąrašą: vardas, pavardė, turimų akcijų kiekis procentais nuo visų akcijų skaičiaus. Pademonstruoti veikimą.
  */
+
+class akc {
+    public $sar = [];
+    function add($va, $pa, $sk){
+        $this->sar[] = [
+          'va' => $va,
+          'pa' => $pa,
+          'sk' => $sk
+        ];
+        usort($this->sar, function($a, $b){
+           if ($a['sk'] > $b['sk']) return -1;
+           elseif ($a['sk'] < $b['sk']) return 1;
+           else 0;
+        });
+    }
+    function info(){
+        $sum = 0;
+        foreach($this->sar as $a){
+            $sum += $a['sk'];
+        }
+        echo '<table>';
+        foreach($this->sar as $a){
+            echo '<tr>';
+            echo '<td>' . $a['va'] . '</td>';
+            echo '<td>' . $a['pa'] . '</td>';
+            echo '<td>' . round(($a['sk'] / $sum) * 100, 0) . '%</td>';
+            echo '</tr>';
+        }
+        echo '</table>';
+    }
+}
+
+$o = new akc();
+$o->add('Jonas', 'Jonaitis', 70);
+$o->add('Petras', 'Petraitis', 200);
+$o->add('Antanas', 'Antanaitis', 30);
+var_dump($o);
+$o->info();
