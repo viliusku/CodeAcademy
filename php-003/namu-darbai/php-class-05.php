689a0698af6356f768f486bd277da89f79738a2b
viliusku <viliusku@gmail.com>
2018-04-25 10:05:29 +0300

    vk

---
+++ php-003/namu-darbai/php-class-05.php
@@ -0,0 +1,5 @@
+<?php
+/**
+Sukurkite a klasę su savybe - masyvu. Sukurkite metdą, su kuriuo būtų galima patalpinti į masyvą atsitiktini skaičių intervale nuo 1 iki 100. Sukurkite metodą, kuris suskaičiuoja ir gražina nelyginių skaičių kiekį masyve. Sukurkite b klasę, kuri paveldi a klasę ir joje sukurkite metodą, kuris suskaičiuoja ir gražina lyginių skaičių kiekį. Sukurkite b klasės egzempliorių ir pademonstrukokite visų 3 metodų veikimą.
+ *
+ */
---
+++ php-003/namu-darbai/php-class-06.php
@@ -0,0 +1,4 @@
+<?php
+/**
+Sukurkite klasę su prekių sąrašu. Sukurkite metodą, kurio pagalba galima patalpintį į sąrašą duomenis apie prekes: pavadinimas, kaina, kiekis. Sukurkite metodą, kuris gražina prekių sąrašą su 3 pigiausiomis prekėmis. Pademonstruokite veikimą.
+ */
