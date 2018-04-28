0bd8f932ae59f537f1a779e3c64a88a9d3eed228
viliusku <viliusku@gmail.com>
2018-04-28 15:39:09 +0300

    vk

---
+++ php-003/namu-darbai/php-form-class-01.php
@@ -0,0 +1,4 @@
+<?php
+/**
+Sukurti POST formą, kuri leistų įvesti svečio duomenis: vardas, pavardė, pasirinkti patiekalą. Sukurti formos PHP backend, kuri priimtų formos duomenis ir juos išvestų ekrane. Įdėti nuorodą į formą, kad būtų galima grįžti ir įvesti naują svečią. Backend suprogramuoti naudojant class.
+ */
---
+++ php-003/namu-darbai/php-form-class-02.php
@@ -0,0 +1,4 @@
+<?php
+/**
+Sukurti GET formą, kuri leistų įvesti automobilio duomenis: gamintojas, modelis, kuro rūšis (iš sąrašo), daužta/nedaužta (radio), metai, kaina. Sukurti formos backend, kuris priimtų formos duomenis ir įrašytų į masyvą, kuris yra sesijoje. Išvesti visus sesijoje esančio masyvo duomenis apie automobilius lentele. Įdėti nuorodą į formą, kad būtų galima grižti ir įvesti naują automobilį. Backend suprogramuoti naudojant class.
+ */
