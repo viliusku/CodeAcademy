b3d0462bf08bbcfaabda11b6f1d245df9bffc58d
viliusku <viliusku@gmail.com>
2018-05-01 11:25:01 +0300

    vk

---
+++ php-003/namu-darbai/php-file-01.php
@@ -0,0 +1,4 @@
+<?php
+/**
+Sukurti skaičių masyvą, paversti masyvą tekstu, sujungiant elementus kableliu ir įrašyti į failą. Su teksto redaktoriumi papildyti failo turinį keletu naujų skaičių. Parašyti PHP skriptą, kuris nuskaito pakeistą failą, atstato iš jo masyvą. Išvesti masyvo turinį su var_dump.
+ */
---
+++ php-003/namu-darbai/php-file-02.php
@@ -0,0 +1,4 @@
+<?php
+/**
+Tekstinio redaktoriaus pagalba sukurti failą su teleloto skaičiais (5 x 5). Parašyto PHP skriptą, kuris nuskaito failą ir išveda ekrane teleloto lentelę (5 x 5).
+ */
