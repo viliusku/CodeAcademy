128844b8774d017b328c1b7c9c2020d290d0d9c1
viliusku <viliusku@gmail.com>
2018-05-01 11:20:01 +0300

    vk

--- php-003/formos/backend-post-file.php
+++ php-003/formos/backend-post-file.php
@@ -1,44 +1,35 @@
 <?php
 
 class forma {
-    private $fn;
-    function __construct($fn){
-        $this->fn = $fn;
-    }
-    function add(){
-        if (file_exists($this->fn)){
-            $f = fopen($this->fn, 'r');
-            $s = fread($f, filesize($this->fn));
-            $m = json_decode($s, true);
-            fclose($f);
-        } else $m = [];
-        $m[] = $_POST;
-        $f = fopen($this->fn, 'a');
-        fwrite($f, json_encode($m));
-        fclose($f);
-    }
-    function info(){
-        if (file_exists($this->fn)){
-            $f = fopen($this->fn, 'r');
-            $s = fread($f, filesize($this->fn));
-            $m = json_decode($s, true);
-            fclose($f);
-        } else $m = [];
-
-        if (count($m)>0){
-            echo '<table>';
-            foreach ($m as $asmuo) {
-                echo '<tr>';
-                echo '<td>' . $asmuo['vardas'] . '</td>';
-                echo '<td>' . $asmuo['lytis'] . '</td>';
-                echo '</tr>';
-            }
-            echo '</table>';
+    private $fn; // tekstinio failo pavadinimas, perduodamas per konstruktorių
+    private $m = []; // asmenų masyvas, kuris nuskaitomas iš failo, papildomas ir išasaugojamas į failą
+    function __construct($fn){ // konstruktorius
+        $this->fn = $fn; // išsaugoti failo pavadimimą
+        if (file_exists($this->fn)){ // jeigu failas yra (pirmą kartą jo dar nėra, todėl būtų klaida skaitant)
+            $f = fopen($this->fn, 'r'); // atsidaryti failą skaitymui
+            $s = fread($f, filesize($this->fn)); // nuskaityti iš failo tekstą
+            $this->m = json_decode($s, true); // dekoduoti iš teksta (json formate) į masyvą
+            fclose($f); // uždaryti failą
         }
     }
+    function add(){ // papildyti asmenų masyvą
+        $this->m[] = $_POST; // pridėti POST formos duomenys (asociatyvinį masyvą) į asmenų masyvą
+        $f = fopen($this->fn, 'w'); // atsidaryti failą rašymui
+        fwrite($f, json_encode($this->m)); // užkoduoti asmenš masyvą į tekstą (json formatu) ir išsaugoti faile
+        fclose($f); // uždaryti failą
+    }
+    function info(){ // asmenų masyvo atvaizdavimas lentele
+        echo '<table>';
+        foreach ($this->m as $asmuo) { // imti po vieną asmenį iš masyvo ir atvaizduoti lentele
+            echo '<tr>';
+            echo '<td>' . $asmuo['vardas'] . '</td>';
+            echo '<td>' . $asmuo['lytis'] . '</td>';
+            echo '</tr>';
+        }
+        echo '</table>';
+    }
 }
-$o = new forma('asmenys.txt');
-$o->add();
-$o->info();
-
+$o = new forma('asmenys.txt'); // susikurti klasės egzempliorių
+$o->add(); // įdėti asmens POST formos duomenis į masyvą (ir išsaugoti faile)
+$o->info(); // atvaizduoti asmenų sąrašą lentele
 echo '<a href="frontend-post-file.html">Atgal</a>';
