416259671dc5e7ecb7943772c1150b9f8f9870e3
viliusku <viliusku@gmail.com>
2018-05-04 19:05:01 +0300

    vk

--- php-003/namu-darbai/php-file-02-2.php
+++ php-003/namu-darbai/php-file-02-2.php
@@ -3,10 +3,16 @@
 Tekstinio redaktoriaus pagalba sukurti failą su teleloto skaičiais (5 x 5). Parašyto PHP skriptą, kuris nuskaito failą ir išveda ekrane teleloto lentelę (5 x 5).
  */
 
-$f = fopen('php-file-02.txt', 'r');
-$s = fread($f, filesize('php-file-02.txt'));
+$f = fopen('php-file-02-2.txt', 'r');
+$s = fread($f, filesize('php-file-02-2.txt'));
 fclose($f);
-$m = json_decode($s);
+
+$m = explode("\r\n", $s);
+for ($i=0; $i<count($m); $i++){
+    $m[$i] = explode(' ', $m[$i]);
+}
+//var_dump($m);
+
 echo '<table>';
 foreach($m as $row){
     echo '<tr>';
