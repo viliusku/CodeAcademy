1d03a9f17b3a1a1fe3ecaf605140c6be0856ab81
viliusku <viliusku@gmail.com>
2018-05-04 21:21:04 +0300

    vk

--- php-003/pdo/pdo-next.php
+++ php-003/pdo/pdo-next.php
@@ -1,4 +1,6 @@
 <?php
+error_reporting( E_ALL );
+ini_set('display_errors', '1');
 
 try {
     $cnn = new PDO('mysql:host=localhost;dbname=bandymas', 'bandymas', 'IsmokKodint333'); // rysys su db atidaromas
