6e929320870eaa9bedc031dbccaee9f302a1f6f1
viliusku <viliusku@gmail.com>
2018-05-04 21:18:45 +0300

    vk

--- php-003/pdo/pdo-next.php
+++ php-003/pdo/pdo-next.php
@@ -1,9 +1,15 @@
 <?php
 
-$cnn = new PDO('mysql:host=localhost;dbname=bandymas', 'bandymas', 'IsmokKodint333'); // rysys su db atidaromas
+try {
+    $cnn = new PDO('mysql:host=localhost;dbname=bandymas', 'bandymas', 'IsmokKodint333'); // rysys su db atidaromas
+    $cnn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); // allow exceptions
 
-$res = $cnn->query("select aut_gamintojas as g, aut_modelis as m, aut_metai as mm, aut_kaina as k  from aut order by aut_kaina");
+    $res = $cnn->query("select aut_gamintojas as g, aut_modelis as m, aut_metai as mm, aut_kaina as k  from aut order by aut_kaina");
 
-while ($row = $res->fetch()){
-    echo $row['g'] . ' ' . $row['m']  . ' ' . $row['mm'] . ' ' . $row['k'] . '<br>';
+    while ($row = $res->fetch()) {
+        echo $row['g'] . ' ' . $row['m'] . ' ' . $row['mm'] . ' ' . $row['k'] . '<br>';
+    }
+}
+catch(PDOException $e) {
+    echo $e->getMessage();
 }
