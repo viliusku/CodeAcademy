25e9660b8c9583f74fa6c84eed32a5dfa3848523
viliusku <viliusku@gmail.com>
2018-05-04 18:44:07 +0300

    vk

--- php-003/namu-darbai/php-file-01-read.php
+++ php-003/namu-darbai/php-file-01-read.php
@@ -2,10 +2,8 @@
 /**
 Sukurti skaičių masyvą, paversti masyvą tekstu, sujungiant elementus kableliu ir įrašyti į failą. Su teksto redaktoriumi papildyti failo turinį keletu naujų skaičių. Parašyti PHP skriptą, kuris nuskaito pakeistą failą, atstato iš jo masyvą. Išvesti masyvo turinį su var_dump.
  */
-
-$m = [1, 4, 7, 11, 23, 55];
-$s = implode(',', $m);
-$f = fopen('php-file-01.txt', 'w');
-fwrite($f, $s);
+$f = fopen('php-file-01.txt', 'r');
+$s = fread($f, filesize('php-file-01.txt'));
 fclose($f);
-echo 'ok';
+$m = explode(',', $s);
+var_dump($m);
