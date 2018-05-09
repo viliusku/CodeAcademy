26cdd4c2ee064d8287a53ac7725c26b42ee810e1
viliusku <viliusku@gmail.com>
2018-05-09 10:21:08 +0300

    vk

---
+++ php-003/namu-darbai/php-form-class-03.php
@@ -0,0 +1,4 @@
+<?php
+/**
+Sukurkite klasę personalas su savybe sąrasas. Sukurkite metodą, kurio pagalba būtų galima patalpinti darbuotojo duomenis į sąrašą: pavardė, vardas, pareigos, atlygis ("į rankas"). Padaryti metodą, kuris suskaičiuoja ir išveda lentele:
+ */
