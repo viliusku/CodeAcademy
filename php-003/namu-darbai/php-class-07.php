830f76435baf4bbd9fd4147a30e299456ed4fe16
viliusku <viliusku@gmail.com>
2018-05-09 10:26:55 +0300

    vk

---
+++ php-003/namu-darbai/php-class-07.php
@@ -0,0 +1,4 @@
+<?php
+/**
+Sukurkite klasę klasę asmuo su savybėmis: vardas, pavarde, amžius (reikšmės suteikiamos konstruktoriaus pagalba). Sukurkite klases: darbuotojas ir studentas, kurios paveldi klasę asmuo. Klasė asmuo turi metodą info, kuris išveda teksto eilutę: "Vardas, pavardė, amčius" (duomenis įstatyti iš savybių). Darbuotojas turi papildomas savybes: pareigos, atlyginimas. Studentas turti papildomą savybę: specialybė. Darbuotojo atveju metodas info turi išvesti tekstą: "Darbuotojas: vardas, pavardė, amžius, pareigos, atlyginimas" (reikšmes įstatyti iš savybių). Studento atveju metodas info turi išvesti tekstą: "Studentas: vardas, pavardė, amžius, specialybė" (reikšmes įstatyti iš savybių).
+ */
---
+++ php-003/namu-darbai/php-form-class-03.php
@@ -0,0 +1,4 @@
+<?php
+/**
+Sukurti formą + php (viename faile - nereikia nurodyti formoje action), kuriame sukurti individualios veiklos mokesčių skaičiuoklę pagal https://www.tax.lt/skaiciuokles/individualios_veiklos_mokesciu_skaiciuokle (tik už 2018 metus).
+ */
