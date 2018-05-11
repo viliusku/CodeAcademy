122e962973818690958ff9ded2defd8d22c6bd38
viliusku <viliusku@gmail.com>
2018-05-11 20:52:11 +0300

    vk

---
+++ php-003/kontroliniai/10.4.php
@@ -0,0 +1,4 @@
+<?php
+/**
+Sukurti klasę, su savybe sąrašas. Sukurti metodą, kurio pagalba galima būtų įdėti norimą kiekį skaičių (funkcijos parametras turi būti masyvas). Sukurti metodą, kuris gražina didžiausią skaičių iš sąrašo. Sukurti metodą, kuri gražina mažiausią skaičių iš sąrašo. Pademonstruoti veikimą.
+ */
