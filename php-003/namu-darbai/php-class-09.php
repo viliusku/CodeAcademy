a9d61e240014bf594418ce8c1425b7d3fdac5f8e
viliusku <viliusku@gmail.com>
2018-05-13 10:26:16 +0300

    vk

---
+++ php-003/namu-darbai/php-class-09.php
@@ -0,0 +1,4 @@
+<?php
+/**
+Sukurti klasę su savybe skaičiai (masyvas). Sukurti metodą, kurio pagalba būtų galima papildyti skaičių masyvą nustatytų kiekiu atsitiktiniu skaičių. Atsitiktiniai skaičiai turi būti generuojami pagal nustatytas ribas nuo iki (metodo parametrai: kiek atsitiktinių skaičių, ribos pradžia ir ribos pabaiga). Sukurti metodą, kuri gražina surūšiuotą sąraša skaičių, kurie yra nelyginiai. Pademonstruoti veikimą.
+ */
