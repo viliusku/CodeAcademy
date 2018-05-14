13469b7bf4431863ccfc7968e20100c193d0f096
viliusku <viliusku@gmail.com>
2018-05-14 20:29:07 +0300

    vk

---
+++ php-003/kontroliniai/10.5.php
@@ -0,0 +1,4 @@
+<?php
+/**
+Sukurti klasę, su savybe sąrašas. Sukurti metodą, kurio pagalba galima būtų įdėti studento egzaminų rezultatus: fizika, matematika, programavimas (dšimtbalėje sistemoje). Sukurti metodą, kuris gražina visus egzaminus išlaikiusių studentų sąrašą (egzaminas yra išlaikytas, jeigu rezultatas yra >= 5. Sukurti metodą, kuris gražina egzaminų laikymo vidurkį. Pademonstruoti veikimą.
+ */
