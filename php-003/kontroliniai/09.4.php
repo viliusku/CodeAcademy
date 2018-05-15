c29e3b57a42c00cb3577c87b93a461a5dd21b92f
viliusku <viliusku@gmail.com>
2018-05-15 20:56:40 +0300

    vk

---
+++ php-003/kontroliniai/09.4.php
@@ -0,0 +1,4 @@
+<?php
+/*
+Panaudodami prieš tai sukurtą klasę "miestas", sukurkite klases "didelis" ir "mazas", kurios paveldi "miestas" klasę. "miestas" klasę papildykite metodu “info”, kuris išvestų suformatuotą eilutę “pavadinimas, gyventojų skaičius, regionas”. "didelis" klasėje perrašykite metodą "info", pakeisdami išvedamą eilutę į “pavadinimas, gyventojų skaičius, regionas (didelis miestas)”. didelis miestas ‐ tiesiog paprastas žodis. Patkrinkite visų trijų klasių metodo “info” veikimą.
+*/
