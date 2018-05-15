b176d66301a6531d5c3a7c238f43b34e93209f37
viliusku <viliusku@gmail.com>
2018-05-15 20:52:23 +0300

    vk

---
+++ php-003/kontroliniai/08.4.php
@@ -0,0 +1,4 @@
+<?php
+/*
+Sukurkite PHP skriptą, kuriame būtų aprašyta klasė “miestas”, kuri turi savybes ‐ pavadinimas, gyventojų skaičius, regionas. Sukurkite standartinį klasės __construct metodą, kuriam perdavus tris parametrus ‐ pavadinimas, gyventojų skaičius, regionas ‐ perduotus parametrus padėtų į savo savybes.
+*/
