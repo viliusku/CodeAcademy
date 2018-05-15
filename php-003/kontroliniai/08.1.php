2ecd1897b86cc23183eb191b6ddc40961433433d
viliusku <viliusku@gmail.com>
2018-05-15 21:31:31 +0300

    vk

--- php-003/kontroliniai/08.1.php
+++ php-003/kontroliniai/08.1.php
@@ -2,3 +2,15 @@
 /*
 Sukurkite PHP skriptą, kuriame būtų aprašyta klasė “automobilis”, kuri turi savybes ‐ gamintojas, modelis, metai. Sukurkite standartinį klasės __construct metodą, kuriam perdavus tris parametrus ‐ gamintojas, modelis, metai ‐ perduotus parametrus padėtų į savo savybes.
 */
+
+class automobilis {
+    public $gamintojas, $modelis, $metai;
+    function __construct($gam, $mod, $met){
+        $this->gamintojas = $gam;
+        $this->modelis = $mod;
+        $this->metai = $met;
+    }
+}
+
+$o = new automobilis('Honda', 'Civic', 2005);
+var_dump($o);
--- php-003/kontroliniai/09.1.php
+++ php-003/kontroliniai/09.1.php
@@ -2,3 +2,34 @@
 /*
 Panaudodami prieš tai sukurtą klasę automobilis, sukurkite klases lengvasis ir krovininis, kurios paveldi automobilis klasę. automobilis klasę papildykite metodu “duomenys”, kuris išvestų suformatuotą eilutę “gamintojas - modelis - metai”. lengvasis klasėje perrašykite metodą duomenys, pakeisdami išvedamą eilutę į “gamintojas, modelis, metai (lengvasis automobilis)”. lengvasis automobilis ‐ tiesiog paprastas žodis.
 */
+
+class automobilis {
+    public $gamintojas, $modelis, $metai;
+    function __construct($gam, $mod, $met){
+        $this->gamintojas = $gam;
+        $this->modelis = $mod;
+        $this->metai = $met;
+    }
+    function duomenys(){
+        $s = "%s - %s - %s";
+        echo sprintf($s, $this->gamintojas, $this->modelis, $this->metai);
+    }
+}
+class krovininis extends automobilis {
+
+}
+class lengvasis extends automobilis {
+    function duomenys(){
+        $s = "%s, %s, %s (lengvasis automobilis)";
+        echo sprintf($s, $this->gamintojas, $this->modelis, $this->metai);
+    }
+}
+
+$o = new automobilis('Honda', 'Civic', 2005);
+$o->duomenys();
+echo '<br>';
+$o = new krovininis('Ford', 'Transit', 2008);
+$o->duomenys();
+echo '<br>';
+$o = new lengvasis('BMW', '320i', 2011);
+$o->duomenys();
