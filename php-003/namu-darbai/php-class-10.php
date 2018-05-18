30353ad99445ae721581f9043d500cb4758e5c3b
viliusku <viliusku@gmail.com>
2018-05-18 21:20:23 +0300

    vk

---
+++ php-003/namu-darbai/php-class-10.php
@@ -0,0 +1,5 @@
+<?php
+/**
+Sukurti klasę naujienos su savybe sąrašas. Sukurti metodą, kurio pagalba būtų galima patalpinti naujieną į sąrašą. Naujieną talpinti asociatyviniu masyvu: data, tekstas (tekstas turi būti vienas sakinys).
+ * Sukurti metodą info, kuris: a) surūšiuotų masyvą pagal datą; b) paverstu teksto visus simbolius mažosiomis raidėmis, o pirmąją raidę - dižiąja; c) išvestu visas naujienas lentele.
+ */
