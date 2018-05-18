958be81568ea07b55252b4dd8ce7d1019b03df60
viliusku <viliusku@gmail.com>
2018-05-18 21:11:40 +0300

    vk

--- php-003/kontroliniai/10.6.php
+++ php-003/kontroliniai/10.6.php
@@ -2,3 +2,23 @@
 /*
 Sukurkite PHP skriptą, kuriame aprašykime klasę countryList, kurioje būtų viena savybė ‐ $countries, kuri bus masyvas, taip pat būtų metodas insert($country), kuris papildo savybę $countries nauja šalimi. Taip pat sukurkite metodą average(), kuris grąžintų masyve sąvybėje esančių šalių pavadinimų ilgio matematinį vidurkį
 */
+class countryList {
+    public $countries = [];
+    function insert($country){
+        $this->countries[] = $country;
+    }
+    function average(){
+        $suma = 0;
+        foreach($this->countries as $cntry){
+            $suma += strlen($cntry);
+        }
+        return $suma / count($this->countries);
+    }
+}
+
+$x = new countryList();
+$x->insert('Lietuva');
+$x->insert('Latvija');
+$x->insert('Graikija');
+$x->insert('Moldavija');
+echo $x->average();
