d17f53f362c72e37f837613ab554cbe176370874
viliusku <viliusku@gmail.com>
2018-05-18 20:06:49 +0300

    vk

--- php-003/kontroliniai/08.4.php
+++ php-003/kontroliniai/08.4.php
@@ -2,3 +2,13 @@
 /*
 Sukurkite PHP skriptą, kuriame būtų aprašyta klasė “miestas”, kuri turi savybes ‐ pavadinimas, gyventojų skaičius, regionas. Sukurkite standartinį klasės __construct metodą, kuriam perdavus tris parametrus ‐ pavadinimas, gyventojų skaičius, regionas ‐ perduotus parametrus padėtų į savo savybes.
 */
+class miestas {
+    public $pavadinimas, $skaicius, $regionas;
+    function __construct($p, $s, $r){
+        $this->pavadinimas = $p;
+        $this->skaicius = $s;
+        $this->regionas = $r;
+    }
+}
+$o = new miestas('Kaunas', 300000, 'Aukštaitija');
+var_dump($o);
--- php-003/kontroliniai/09.4.php
+++ php-003/kontroliniai/09.4.php
@@ -2,3 +2,30 @@
 /*
 Panaudodami prieš tai sukurtą klasę "miestas", sukurkite klases "didelis" ir "mazas", kurios paveldi "miestas" klasę. "miestas" klasę papildykite metodu “info”, kuris išvestų suformatuotą eilutę “pavadinimas, gyventojų skaičius, regionas”. "didelis" klasėje perrašykite metodą "info", pakeisdami išvedamą eilutę į “pavadinimas, gyventojų skaičius, regionas (didelis miestas)”. didelis miestas ‐ tiesiog paprastas žodis. Patkrinkite visų trijų klasių metodo “info” veikimą.
 */
+class miestas {
+    public $pavadinimas, $skaicius, $regionas;
+    function __construct($p, $s, $r){
+        $this->pavadinimas = $p;
+        $this->skaicius = $s;
+        $this->regionas = $r;
+    }
+    function info(){
+        $s = "%s, %s, %s";
+        echo sprintf($s, $this->pavadinimas, $this->skaicius, $this->regionas);
+    }
+}
+class mazas extends miestas {
+
+}
+class didelis extends miestas {
+    function info(){
+        $s = "%s, %s, %s (didelis miestas)";
+        echo sprintf($s, $this->pavadinimas, $this->skaicius, $this->regionas);
+    }
+}
+$o = new miestas('Marijampolė', 60000, 'Suvalkija');
+$o->info(); echo '<br>';
+$o = new didelis('Kaunas', 300000, 'Aukštaitija');
+$o->info(); echo '<br>';
+$o = new mazas('Gargždai', 16000, 'Žemaitija');
+$o->info();
