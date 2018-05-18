2bc6581f14b484de14ec5788925090f969e4416d
viliusku <viliusku@gmail.com>
2018-05-18 20:16:50 +0300

    vk

--- php-003/kontroliniai/08.2.php
+++ php-003/kontroliniai/08.2.php
@@ -2,3 +2,13 @@
 /*
 Sukurkite PHP skriptą, kuriame būtų aprašyta klasė “codeAcademy”, kuri turi savybes ‐ data, skaicius, auditorija. Sukurkite standartinį klasės __construct metodą, kuriam perdavus tris parametrus ‐ data, skaicius, auditorija ‐ perduotus parametrus padėtų į savo savybes.
 */
+class codeAcademy {
+    public $data, $skaicius, $auditorija;
+    function __construct($d, $s, $a){
+        $this->data = $d;
+        $this->skaicius = $s;
+        $this->auditorija = $a;
+    }
+}
+$o = new codeAcademy('2018-06-01', 10, 2);
+var_dump($o);
--- php-003/kontroliniai/09.2.php
+++ php-003/kontroliniai/09.2.php
@@ -2,3 +2,34 @@
 /*
 Panaudodami prieš tai sukurtą klasę codeAcademy, sukurkite klases backend ir frontend, kurios paveldi codeAcademy klasę. codeAcademy klasę papildykite metodu “kursas”, kuris išvestų suformatuotą eilutę “data, skaicius, auditorija”. backend klasėje perrašykite metodą duomenys, pakeisdami išvedamą eilutę į “data, skaicius, auditorija (backend kursas)”. backend kursas ‐ tiesiog paprastas žodis. frontend klasėje perrašykite metodą duomenys, pakeisdami išvedamą eilutę į “data, skaicius, auditorija (frontend kursas)”. frontend kursas ‐ tiesiog paprastas žodis. Patkrinkite visų trijų klasių metodo “kursas” veikimą.
 */
+
+class codeAcademy {
+    public $data, $skaicius, $auditorija;
+    function __construct($d, $s, $a){
+        $this->data = $d;
+        $this->skaicius = $s;
+        $this->auditorija = $a;
+    }
+    function kursas(){
+        $s = "%s, %s, %s";
+        echo sprintf($s, $this->data, $this->skaicius, $this->auditorija);
+    }
+}
+class backend extends codeAcademy {
+    function kursas(){
+        $s = "%s, %s, %s (backend kursas)";
+        echo sprintf($s, $this->data, $this->skaicius, $this->auditorija);
+    }
+}
+class frontend extends codeAcademy {
+    function kursas(){
+        $s = "%s, %s, %s (frontend kursas)";
+        echo sprintf($s, $this->data, $this->skaicius, $this->auditorija);
+    }
+}
+$o = new codeAcademy('2018-06-01', 10, 2);
+$o->kursas(); echo '<br>';
+$o = new backend('2018-07-01', 12, 1);
+$o->kursas(); echo '<br>';
+$o = new frontend('2018-08-01', 11, 3);
+$o->kursas();
