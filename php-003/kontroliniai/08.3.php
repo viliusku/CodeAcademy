ebd8689bdaab92d278a229acea7813c9ce372ba8
viliusku <viliusku@gmail.com>
2018-05-18 19:39:16 +0300

    vk

---
+++ php-003/kontroliniai/08.3.php
@@ -0,0 +1,4 @@
+<?php
+/*
+Sukurkite PHP skriptą, kuriame būtų aprašyta klasė “preke”, kuri turi savybes ‐ kodas, pavadinimas, kaina. Sukurkite standartinį klasės __construct metodą, kuriam perdavus tris parametrus ‐ kodas, pavadinimas, kaina padėtų į savo savybes.
+*/
