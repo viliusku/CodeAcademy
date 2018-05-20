d40307b32aac2c87aa100b4657d982046c426f9e
viliusku <viliusku@gmail.com>
2018-05-20 10:53:20 +0300

    vk

---
+++ php-003/namu-darbai/php-class-11.php
@@ -0,0 +1,4 @@
+<?php
+/**
+Sukurkite PHP skriptą, kuriame aprašykime klasę skaičiuoklė, kurioje būtų viena savybė - sarašas, kuri bus masyvas, taip pat būtų metodas add(skaičius), kuris prideda naują skaičių į masyvą sąvybę sarašas. Taip pat sukurkite metodą info(), kuris grąžintų masyve sąvybėje esančių elementų kvadratų aritmetinį vidurkį.
+ */
