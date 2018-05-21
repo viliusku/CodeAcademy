45c3b069cb86e6fa64eb8d53cbcef11521fe7094
viliusku <viliusku@gmail.com>
2018-05-21 17:15:46 +0300

    vk

--- php-003/kontroliniai/10.4.php
+++ php-003/kontroliniai/10.4.php
@@ -2,3 +2,28 @@
 /**
 Sukurti klasę, su savybe sąrašas. Sukurti metodą, kurio pagalba galima būtų įdėti norimą kiekį skaičių (funkcijos parametras turi būti masyvas). Sukurti metodą, kuris gražina didžiausią skaičių iš sąrašo. Sukurti metodą, kuri gražina mažiausią skaičių iš sąrašo. Pademonstruoti veikimą.
  */
+
+class cla {
+    public $sarasas = [];
+    function add($m){
+        foreach($m as $sk) $this->sarasas[] = $sk;
+    }
+    function getMin(){
+        $s = $this->sarasas[0];
+        foreach($this->sarasas as $sk){
+            if ($sk < $s) $s = $sk;
+        }
+        return $s;
+    }
+    function getMax(){
+        $s = $this->sarasas[0];
+        foreach($this->sarasas as $sk){
+            if ($sk > $s) $s = $sk;
+        }
+        return $s;
+    }
+}
+$o = new cla();
+$o->add([1,3,5,6,7,8]);
+$o->add([11,23,24,24,12]);
+echo 'Max: ' . $o->getMax() . ', Min: ' . $o->getMin();
