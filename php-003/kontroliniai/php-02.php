aa612ba8e18aa9f73a8c6c2d5a9402a823f3009f
viliusku <viliusku@gmail.com>
2018-05-21 19:10:08 +0300

    vk

--- php-003/kontroliniai/php-02.php
+++ php-003/kontroliniai/php-02.php
@@ -2,3 +2,33 @@
 /**
 Sukurti klasę svečiai. Klaseje turi būti savybė - svečių sąrašas. Sukurti klasės metodą, su kuriuo būtų galima papildyti svečių sąrašą. Informacija apie svečią: vardas, lytis, patiekalas, amžius. Sukurti klasės metodą svečių sąrašui suformuoti.
  */
+
+class sveciai {
+    public $sarasas = [];
+    function add($vardas, $lytis, $patiekalas, $amzius){
+        $this->sarasas[] = [
+          'vardas' => $vardas,
+          'lytis' => $lytis,
+          'patiekalas' => $patiekalas,
+          'amzius' => $amzius
+        ];
+    }
+    function info(){
+        echo '<table>';
+        foreach ($this->sarasas as $svecias){
+            echo '<tr>';
+            echo '<td>' . $svecias['vardas'] . '</td>';
+            echo '<td>' . $svecias['lytis'] . '</td>';
+            echo '<td>' . $svecias['patiekalas'] . '</td>';
+            echo '<td>' . $svecias['amzius'] . '</td>';
+            echo '</tr>';
+        }
+        echo '</table>';
+    }
+}
+
+$o = new sveciai();
+$o->add('Jonas', 'vyras', 'Mėsa', 30);
+$o->add('Petras', 'vyras', 'Žuvis', 35);
+$o->add('Marytė', 'moteris', 'Vegetariškas', 29);
+$o->info();
--- php-003/kontroliniai/php-03.php
+++ php-003/kontroliniai/php-03.php
@@ -2,3 +2,25 @@
 /**
 Sukurti klasę, kuri turi savybę - skaičių sąrašas. Sukurti metodą, kuris sugeneruotų atsitiktinį skaičių intervale nuo 1 iki 100 ir patalpintų į sąrašą, o po to surūšiuotų sąrašą. Sukurti metodą, kuris gražiną sugeneruotų skaičių aritmetinį vidurkį. Sukurti metodą, kuris išveda masyvo elementus tekstu, kur masyvo elementai atskirti kableliais. Pademonstruoti veikimą.
  */
+class cla {
+    public $skaiciai = [];
+    function add(){
+        $this->skaiciai[] = rand(1, 100);
+        sort($this->skaiciai);
+    }
+    function vidurkis(){
+        $s = 0;
+        foreach($this->skaiciai as $sk) $s += $sk;
+        return $s / count($this->skaiciai);
+    }
+    function tekstas(){
+        return implode(',', $this->skaiciai);
+    }
+}
+$o = new cla();
+$o->add();
+$o->add();
+$o->add();
+$o->add();
+$o->add();
+echo 'Vidurkis: ' . $o->vidurkis() . ', tekstas: ' . $o->tekstas();
