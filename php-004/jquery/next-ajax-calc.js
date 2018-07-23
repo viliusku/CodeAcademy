55fee7f7500b58b03610ac0791f77c909c4e9035
viliusku <viliusku@gmail.com>
2018-07-23 21:24:37 +0300

    vk

--- php-004/jquery/next-ajax-calc.js
+++ php-004/jquery/next-ajax-calc.js
@@ -1,17 +1,18 @@
 $(document).ready(function(){
     $('#frm input[type=checkbox]').click(function(){
         var els = $('#frm input:checked');
-        var data = {};
+        var parametrai = {};
         for (var i=0; i<els.length; i++){
-            data[$(els[i]).attr('name')] = $(els[i]).val();
+            parametrai[$(els[i]).attr('name')] = $(els[i]).val();
         }
+        //alert(JSON.stringify(data));
         $.ajax({
             method: "POST",
             url: "next-ajax-calc.php",
             dataType: 'json',
-            data: data,
-            success: function(data){
-                $('#frm input[name=kaina]').val(data);
+            data: parametrai,
+            success: function(gauta){
+                $('#frm input[name=kaina]').val(gauta);
             }
         });
     });
