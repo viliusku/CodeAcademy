56950063f804e8d78afde76f68d93f8a190bd703
viliusku <viliusku@gmail.com>
2018-08-14 20:42:35 +0300

    vk

--- php-005/namu-darbai/php-04.php
+++ php-005/namu-darbai/php-04.php
@@ -1,5 +1,4 @@
 <?php
 /**
-
- Yra indeksinis (paprastas) žmonių masyvas, kur kiekvienas žmogus ura kaip asociatyvinis masyvas: vardas, lytis. Suskaičiuoti kiek vyrų ir kiek moterų.
+ Yra indeksinis (paprastas) žmonių masyvas, kur kiekvienas žmogus yra kaip asociatyvinis masyvas: vardas, lytis. Suskaičiuoti kiek vyrų ir kiek moterų. Naudoti foreach.
  */
