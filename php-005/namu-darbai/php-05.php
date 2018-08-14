09a21a8346250fca25d433bbe1710f944b849dcb
viliusku <viliusku@gmail.com>
2018-08-14 21:29:53 +0300

    vk

---
+++ php-005/namu-darbai/php-05.php
@@ -0,0 +1,4 @@
+<?php
+/**
+Yra sąrašas darbuotojų, kurių duomenys yra šie: vardas, stazas, atlyginimas, priedas. Paskaičiuoti darbuotojų priedus pagal sekančias taisykles: a) jeigu stazas iki 5 metų, tai priedas 5%, jeigu stazas virš 5 metų, tai priedas 10%, o jeigu stažas virš 10 metų, tai priedas 15%. I6vesti sąrašą su var_dump.
+ */
