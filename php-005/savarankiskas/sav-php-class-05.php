522ba89345f809cae3b7adf2f7266321df19ec9b
viliusku <viliusku@gmail.com>
2018-08-23 20:27:18 +0300

    vk

--- php-005/savarankiskas/sav-php-class-05.php
+++ php-005/savarankiskas/sav-php-class-05.php
@@ -1,4 +1,4 @@
 <?php
 /**
-Sukrti klasę ir metoda joje. Klasė turi turėti savybę - masyvą. Metodo parametrai: tekstas, skaičius. Funkcija turi atsitiktiniu būdu išmaišyti teksto simbolius ir įdėti į masyvą tiek kartų, kiek nurodyta antrame parametre. Suū var dump išvesti klasės ekzemplioriaus turinį.
+Sukrti klasę ir metoda joje. Klasė turi turėti savybę - masyvą. Metodo parametrai: tekstas, skaičius. Funkcija turi atsitiktiniu būdu išmaišyti teksto simbolius ir įdėti į masyvą tiek kartų, kiek nurodyta antrame parametre. Su var_dump išvesti klasės ekzemplioriaus turinį.
  */
