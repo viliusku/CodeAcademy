14ea75bb0b9457dfa6a525366dda6e73822d08de
viliusku <viliusku@gmail.com>
2018-08-23 21:15:55 +0300

    vk

---
+++ php-005/namu-darbai/php-klase-02.php
@@ -0,0 +1,5 @@
+<?php
+/**
+Sukurti klasę. Klasėje turi būti savybe - automobiliu masyvas: gamintojas, modelis, metai, kaina. masyve turi būti duomenys. Sukurti funkciją, kuri paskaičiuotų kainos vidurkį ir grazintų atgal. Išvesti kainos vidurkį.
+
+ */
