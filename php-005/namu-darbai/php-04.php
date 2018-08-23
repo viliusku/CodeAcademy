f53b72377ae2a81107bf68b084ab8065ade97697
viliusku <viliusku@gmail.com>
2018-08-23 19:01:43 +0300

    vk

--- php-005/namu-darbai/php-04.php
+++ php-005/namu-darbai/php-04.php
@@ -2,3 +2,31 @@
 /**
  Yra indeksinis (paprastas) žmonių masyvas, kur kiekvienas žmogus yra kaip asociatyvinis masyvas: vardas, lytis. Suskaičiuoti kiek vyrų ir kiek moterų. Naudoti foreach.
  */
+
+$mas = [
+    ['vardas' => 'Jonas', 'lytis' => 'vyras'],
+    ['vardas' => 'Petras', 'lytis' => 'vyras'],
+    ['vardas' => 'Antanas', 'lytis' => 'vyras'],
+    ['vardas' => 'Marytė', 'lytis' => 'moteris'],
+    ['vardas' => 'Auksė', 'lytis' => 'moteris'],
+    ['vardas' => 'Elena', 'lytis' => 'moteris']
+];
+
+function moteru($m){
+    $s = 0;
+    foreach($m as $z){
+        if ($z['lytis']=='moteris') $s++;
+    }
+    return $s;
+}
+
+function vyru($m){
+    $s = 0;
+    foreach($m as $z){
+        if ($z['lytis']=='vyras') $s++;
+    }
+    return $s;
+}
+
+$s = "Vyrų: %s, moterų: %s";
+echo sprintf($s, vyru($mas), moteru($mas));
--- php-005/namu-darbai/php-klase-01.php
+++ php-005/namu-darbai/php-klase-01.php
@@ -2,3 +2,36 @@
 /**
 Sukurti klasę, kurioje yra savybė masyvas su asmenimis, kur kiekvienas asmuo yra asociatyvins masyvas: vardas, lytis. Sukurti dvi funkcijas, kurios paskaičiuotų kiek yra moterų ir kiek yra vyrų ir gražintų reikšmes. Pademonstruoti veikimą.
  */
+
+class abc {
+    public $mas = [
+        ['vardas' => 'Jonas', 'lytis' => 'vyras'],
+        ['vardas' => 'Petras', 'lytis' => 'vyras'],
+        ['vardas' => 'Antanas', 'lytis' => 'vyras'],
+        ['vardas' => 'Marytė', 'lytis' => 'moteris'],
+        ['vardas' => 'Auksė', 'lytis' => 'moteris'],
+        ['vardas' => 'Elena', 'lytis' => 'moteris']
+    ];
+
+    function moteru()
+    {
+        $s = 0;
+        foreach ($this->mas as $z) {
+            if ($z['lytis'] == 'moteris') $s++;
+        }
+        return $s;
+    }
+
+    function vyru()
+    {
+        $s = 0;
+        foreach ($this->mas as $z) {
+            if ($z['lytis'] == 'vyras') $s++;
+        }
+        return $s;
+    }
+}
+$o = new abc();
+echo sprintf("Vyrų: %s, moterų: %s", $o->vyru(), $o->moteru());
+
+//echo "Vyrų: " . $o->vyru() . ", moterų: " . $o->moteru();
