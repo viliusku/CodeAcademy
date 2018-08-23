f51d7e1a8f3e23c7877b17a7f40fda48c5f716dc
viliusku <viliusku@gmail.com>
2018-08-23 19:48:45 +0300

    vk

--- php-005/savarankiskas/sav-php-class-03.php
+++ php-005/savarankiskas/sav-php-class-03.php
@@ -1,4 +1,4 @@
 <?php
 /**
-Sukurti klasę ir joje metodą, kuris paskaičiuotų dviejų skaičių suma, kitų dviejų skaičių suma ir gražintų skirtumą tarp pirmos ir antros.
+Sukurti klasę ir joje metodą, kuris paskaičiuotų dviejų skaičių suma, kitų dviejų skaičių suma ir gražintų skirtumą tarp pirmos ir antros sumų.
  */
