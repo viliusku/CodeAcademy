9953a0fe3a0ef32ae3e1a48aafb84ae18889acbb
viliusku <viliusku@gmail.com>
2018-08-24 19:07:34 +0300

    vk

---
+++ php-005/savarankiskas/sav-php-class-06.php
@@ -0,0 +1,4 @@
+<?php
+/**
+Skurti klasę su konstruktorium, kuris turi parametrą - atstiktinių skaičių kiekis. Konstruktorius turi sugeneruoti atsitiktinius skaičius savybėje - masyve. Sukurti funkciją, kuri paskaičiuotų skaičių vidurkį ir jį gražintų. Pademonstruoti veikimą.
+ */
