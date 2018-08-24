9cb8c072a97db23434a009a7bbd74a1c167b22e5
viliusku <viliusku@gmail.com>
2018-08-24 19:41:08 +0300

    vk

---
+++ php-005/savarankiskas/sav-php-class-07.php
@@ -0,0 +1,4 @@
+<?php
+/**
+Yra klasė su savybe - masyvu. Konstruktoriaus pagalba sugeneruoti n atsitiktinių skaičių intervale nuo a iki b. metodo pagalba suskaičiuoti nelyginių skaičių sumą ir gražinti ją. Pademonstruoti veikimą.
+ */
