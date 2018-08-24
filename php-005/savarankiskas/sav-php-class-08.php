b4c0e62829aa96967d496375d4fcd828e5724dd2
viliusku <viliusku@gmail.com>
2018-08-24 20:18:29 +0300

    vk

---
+++ php-005/savarankiskas/sav-php-class-08.php
@@ -0,0 +1,4 @@
+<?php
+/**
+ Surašyti skaičius į masyvą. Sukurti klasę ir savybę - masyvą; Konstruktoriaus pagalba perduoti skaičių masyvą į klasės savybę. Sukurti funkciją, kuri sumuoja kas antrą masyvo skaičių ir sumą gražina. Pademonstruoti veikimą.
+ */
