eb7627e7238c535036d466bad331f945ab58a3bc
viliusku <viliusku@gmail.com>
2018-08-26 10:39:28 +0300

    vk

---
+++ php-005/namu-darbai/php-10.php
@@ -0,0 +1,4 @@
+<?php
+/**
+Sukurti 3 kintamuosius ir jiems priskirti reikšmes. Kintamuojų reikšmes pakelti kvadratu, sudėti ir ištraukti kvadratinę šaknį. Rezultatą įrašyti į naują kintamajį. Išvesti su echo.
+ */
