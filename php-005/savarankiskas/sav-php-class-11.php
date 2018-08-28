7532acab5e22732d3dc9b02a7e63b5638fb3c53e
viliusku <viliusku@gmail.com>
2018-08-28 21:04:57 +0300

    vk

---
+++ php-005/savarankiskas/sav-php-class-11.php
@@ -0,0 +1,4 @@
+<?php
+/**
+Sukrti klasę auto su savybėmis: gamintojas, modelis, metai, kaina. Sukurti konstruktorių, kurio pagalba būtų galima užpildyti savybes. Sukurti kitą klasę turgus su savybe masinos. Sukurti antrai klasei metodą, kurio pagalba būtų galima perduoti automobilio duomenis, sukurti pagal šiuos duomenis pirmos klasės egzempliorių ir patalpinti į savybę masinos. Sukurti antrai klasei metodą - vidurkis, kuris paskaiciuotu turguje esančių mašinų vidutinę vertę ir gražintų. Pademonstruoti veikimą.
+ */
