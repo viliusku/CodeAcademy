9c0243f26439e0f00a598e2b0ba59d0716393be8
viliusku <viliusku@gmail.com>
2018-08-28 20:26:01 +0300

    vk

---
+++ php-005/savarankiskas/sav-php-class-10.php
@@ -0,0 +1,4 @@
+<?php
+/**
+Sukurti klasę su savybe - masyvas. Sukurti metodą, kurio pagalba būtų galima patalpinti skaičių i klases savybę - masyvą. Sukurti metodą kuris gražintų visų masyvo skaičių sumą. Sukurti kitą klasę, kuri paveldėtų pirmą klasę ir joje perrašyti metodą, kuris gražintų nelyginių skaičių sumą. Sukurti trečią klasę, kuri paveldėtų pirmą klasę ir perrašyti metodą, kuris gražintų visų lyginių skaičių sumą. Pademonstruoti visų trijų klasių ir jų metodų veikimą.
+ */
