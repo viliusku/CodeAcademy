baec70be53cf6a35df78f040d8a012a3419d56e3
viliusku <viliusku@gmail.com>
2018-08-30 20:50:27 +0300

    vk

---
+++ php-005/savarankiskas/sav-php-form-01.php
@@ -0,0 +1,4 @@
+<?php
+/**
+Sukurti formą: gamintojas, modelis, metai (select), nauja/naudota (radio), kaina. Su metodu post perduoti duomenis į backend ir sudėti į klasę auto, su atitinkamomis savybėmis. Rezultatą išvesti su var_dump.
+ */
