1e425ad054bf45d64f49b0a1c6982208d3821612
viliusku <viliusku@gmail.com>
2018-09-05 23:46:31 +0300

    vk

---
+++ php-005/namu-darbai/php-forma-03.php
@@ -0,0 +1,4 @@
+<?php
+/*
+Sukurti frontend (method=post) automobilio duomenų įvedimui: gamintojas, modelis, metai, kaina. Forma turėtu būti nusiųsta į backend failą metodu POST. Duomenis patalpinkite į failą. Iš backend failo automatiškai nukreipti (header) į frontend failą ir ten failo turinį atvaizduoti lentele (tik pirmus tris pigiausius automobilius).
+*/
