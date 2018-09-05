9281ddb8adee64664b441fe0913fdc1044365c5f
viliusku <viliusku@gmail.com>
2018-09-05 23:46:57 +0300

    vk

--- php-005/namu-darbai/php-13.php
+++ php-005/namu-darbai/php-13.php
@@ -1,4 +1,4 @@
 <?php
 /**
-Sukurti klasę studentas, su savybe egzaminai. Sukurti kontruktorių, kurio pagalba būtų galima įdėti sudento duomenis į savybes: vardas, pavarde, fakultetas, kursas. Sukurti metodą, kurio pagalba galima būtų įdėti studento egzamino rezultatus: dalykas, įvertinimas (dešimtbalėje sistemoje). Sukurti metodą su parametru, kuris išveda: vardas, pavardė, fakultetas, kursas, išlaikytų egzaminų: skaičius, neišlaikytų egzaminų: skaičius. Skaičius - suskaičiuotas išlaikytų/neišlaikytų egzaminų skaičiai. Išlaiku egzaminas skaitomas tada, kai įvertinimas yra >= 5.
+Sukurti klasę studentas, su savybe egzaminai. Sukurti kontruktorių, kurio pagalba būtų galima įdėti sudento duomenis į savybes: vardas, pavarde, fakultetas, kursas. Sukurti metodą, kurio pagalba galima būtų įdėti studento egzamino rezultatus: dalykas, įvertinimas (dešimtbalėje sistemoje). Sukurti metodą su parametru, kuris išveda: vardas, pavardė, fakultetas, kursas, išlaikytų egzaminų: skaičius, neišlaikytų egzaminų: skaičius. Skaičius - suskaičiuotas išlaikytų/neišlaikytų egzaminų skaičiai. Išlaikytu egzaminas skaitomas tada, kai įvertinimas yra >= 5.
  */
