94dd865f6c38940b0f1b22ae7d9b8a80e9130e25
viliusku <viliusku@gmail.com>
2018-09-11 23:13:37 +0300

    vk

---
+++ php-005/namu-darbai/php-15.php
@@ -0,0 +1,4 @@
+<?php
+/**
+Padaryti skriptą, kuris nuskaito sitemap.xml failą iš www.delfi.lt (https://www.delfi.lt/sitemap.xml) ir suskaičiuoja kiek tame sitemap.xml faile yra pateikta svetainės puslapių (tag'ų sitemap).
+ */
---
+++ php-005/namu-darbai/php-klase-11.php
@@ -0,0 +1,4 @@
+<?php
+/**
+Sukurti klasę softas, su savybe masyvu licencijos (tinkamos licencijos). Sukurti konstruktorių, kurio pagalba būtų galima perduoti licencijų sąrašą į savybę licencijos. Sukurti metodą add, kurio pagalba būtų galima įvesti programinę įrangą, kaip asociatyvinį masyvą su raktais: pavadinimas, licencija ir patalpinti į savybę programos. Sukurti metodą info, kuris pagal parametrą (tinkama/netinkama) išvestų programinės įrangos sąrašą lentele atitinkamai kurios turi tinkamą (yra sąraše licencijos) arba netinkamą (nėra sąraše) licenciją. https://en.wikipedia.org/wiki/Comparison_of_free_and_open-source_software_licenses
+ */
