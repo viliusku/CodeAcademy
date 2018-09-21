da81013e634c00c3bf9ad0e4e9ef95a673e5babc
viliusku <viliusku@gmail.com>
2018-09-21 21:18:49 +0300

    vk

--- php-005/savarankiskas/sav-php-data-01.php
+++ php-005/savarankiskas/sav-php-data-01.php
@@ -2,3 +2,19 @@
 /*
  Sukurti funkciją, kuri išveda visas išeigines dienas (datas) per nurodytą mėnesį (funkcijos parametrai - metai, mėnuo). Išeiginės dienos: šeštadienis ir sekmadienis.
  */
+
+function iseigines($year, $month){
+    $data = new DateTime();
+    $data->setDate($year, $month, 1);
+    do {
+        $w = $data->format("w");
+        $m = $data->format("m");
+        if ($w == 0 || $w == 6){
+            echo $data->format("Y-m-d") . '<br>';
+        }
+        $data->add(new DateInterval('P1D'));
+    }
+    while ($m == $month);
+}
+
+iseigines(2018, 9);
